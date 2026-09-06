64fb122de7dd834c42964e78829102629836214f
anas-araid <araid.anas99@gmail.com>
2026-09-06 17:53:36 +0200

    feat: integrate vitest with storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-/// <reference types="vitest" />
-
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    include: ["src/**/*.test.{js,ts,tsx}"],
-    globals: true, //https://vitest.dev/guide/migration.html#globals-as-a-default
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.ts",
-  },
 });
